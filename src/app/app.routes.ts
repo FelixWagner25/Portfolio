d4cf5447780f8c7b7cf406ca5c672c0d9a9e74fc
Felix Wagner <post@felixwagner.eu>
2026-02-27 14:01:24 +0100

    feat: wire routes in router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { Imprint } from './imprint/imprint';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: LandingPage},
+    {path: "imprint", component: Imprint},
+    {path: "privacy-policy", component: PrivacyPolicy},
+    {path: "page-not-found", component: PageNotFound},
+    {path: "**", redirectTo: "page-not-found"}
+];
