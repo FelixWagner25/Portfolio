2f6ffecb3cd8736ade1f2d737c23bcfdfe67617a
Felix Wagner <post@felixwagner.eu>
2026-03-01 10:37:26 +0100

    feat: add favicon and components to main

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { LandingPage } from '../landing-page/landing-page';
 import { AboutMe } from '../about-me/about-me';
+import { MySkills } from '../my-skills/my-skills';
+import { MyPortfolio } from '../my-portfolio/my-portfolio';
+import { Contact } from '../contact/contact';
 
 @Component({
   selector: 'app-main',
-  imports: [LandingPage, AboutMe],
+  imports: [LandingPage, AboutMe, MySkills, MyPortfolio, Contact],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
