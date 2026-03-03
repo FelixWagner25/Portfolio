4fca77218609e987279530d394bd6c6a35d295b0
Felix Wagner <post@felixwagner.eu>
2026-03-03 10:32:34 +0100

    feat: header button background styles and material installation changes

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
