c8258058dee6499d94d5de893369e8ca92a3181f
Felix Wagner <post@felixwagner.eu>
2026-03-08 19:20:43 +0100

    feat: add material menu to header section

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { AppLanguage, LanguageService } from '../services/language.service';
 import { AsyncPipe } from '@angular/common';
+import { MatMenu, MatMenuTrigger, MatMenuItem } from '@angular/material/menu';
+import { MatIconButton } from '@angular/material/button';
 
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatMenuTrigger, MatIconButton, MatMenu, MatMenuItem],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
