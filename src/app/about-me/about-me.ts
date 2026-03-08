83386ce32238b58bcfe8f3d591806826ca381c07
Felix Wagner <post@felixwagner.eu>
2026-03-08 11:07:28 +0100

    feat: implement language switch at about me section

--- src/app/about-me/about-me.ts
+++ src/app/about-me/about-me.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../shared/services/language.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-about-me',
-  imports: [],
+  imports: [CommonModule,AsyncPipe],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
 export class AboutMe {
-
+  private languageService = inject(LanguageService);
+  language$ = this.languageService.language$;
 }
