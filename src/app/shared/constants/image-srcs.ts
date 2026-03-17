4fab3ba84a006ae643b41aa9a2e5289992703246
Felix Wagner <post@felixwagner.eu>
2026-03-17 08:36:54 +0100

    feat: add background-images urls for scroll arrow and menu button

---
+++ src/app/shared/constants/image-srcs.ts
@@ -0,0 +1,19 @@
+export const scrollDownArrowImages = [
+    "none",
+    "url('assets/scroll-down-arrow/variant_2.svg')",
+    "url('assets/scroll-down-arrow/variant_3.svg')",
+    "url('assets/scroll-down-arrow/variant_4.svg')",
+    "url('assets/scroll-down-arrow/variant_5.svg')",
+    "url('assets/scroll-down-arrow/variant_6.svg')",
+    "none",
+]
+
+export const menuBtnImages = [
+    "assets/img/header-menu-btn/variant_1.svg",
+    "assets/img/header-menu-btn/variant_2.svg",
+    "assets/img/header-menu-btn/variant_3.svg",
+    "assets/img/header-menu-btn/variant_4.svg",
+    "assets/img/header-menu-btn/variant_6.svg",
+    "assets/img/header-menu-btn/variant_7.svg",
+    "assets/img/header-menu-btn/variant_8.svg",
+]
