bcae79eaab0e263797aa3ed5360cf949f102c932
Felix Wagner <post@felixwagner.eu>
2026-03-24 14:49:42 +0100

    feat: add and style project descriptions in portfolio section

--- src/app/my-portfolio/my-portfolio.ts
+++ src/app/my-portfolio/my-portfolio.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../shared/services/language.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-my-portfolio',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './my-portfolio.html',
   styleUrl: './my-portfolio.scss',
 })
 export class MyPortfolio {
-
+  private languageService = inject(LanguageService);
+  language$ = this.languageService.language$;
 }
