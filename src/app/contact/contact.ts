7686dc5ec605f06a8efb229c34a580f4dfd4c1df
Felix Wagner <post@felixwagner.eu>
2026-03-30 17:13:11 +0200

    feat: style checkox and button of contact form

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -3,12 +3,13 @@ import { LanguageService } from '../shared/services/language.service';
 import { Component, inject } from '@angular/core';
 import { MatFormField, MatLabel } from '@angular/material/select';
 import { MatInput } from '@angular/material/input';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 
 @Component({
   standalone: true,
   selector: 'app-contact',
-  imports: [AsyncPipe, MatInput, MatFormField, MatLabel],
+  imports: [AsyncPipe, MatInput, MatFormField, MatLabel, MatCheckbox],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
