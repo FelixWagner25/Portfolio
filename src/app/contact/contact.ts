0c45a89716e2a741ddb5362d356a800cf34cf817
Felix Wagner <post@felixwagner.eu>
2026-03-31 13:51:24 +0200

    feat: implement email input validation following angular docs in contact form

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,19 +1,41 @@
 import { AsyncPipe } from '@angular/common';
 import { LanguageService } from '../shared/services/language.service';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatFormField, MatLabel } from '@angular/material/select';
-import { MatInput } from '@angular/material/input';
+import { MatInput, MatError } from '@angular/material/input';
 import { MatCheckbox } from '@angular/material/checkbox';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
 
 
 @Component({
   standalone: true,
   selector: 'app-contact',
-  imports: [AsyncPipe, MatInput, MatFormField, MatLabel, MatCheckbox],
+  imports: [AsyncPipe, MatInput, MatFormField, MatLabel, MatCheckbox, FormsModule, ReactiveFormsModule, MatError],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Contact {
   private languageService = inject(LanguageService);
   language$ = this.languageService.language$;
+
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = signal('');
+
+  constructor(){
+    merge(this.email.statusChanges, this.email.valueChanges).pipe(takeUntilDestroyed()).subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage(){
+    if (this.email.hasError('required')){
+      this.errorMessage.set('Your email address is required');
+    } else if (this.email.hasError('email')){
+      this.errorMessage.set('Not a valid email address');
+    } else{
+      this.errorMessage.set('');
+    }
+  }
 }
