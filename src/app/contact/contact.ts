6597b966ea7d0335d23833c076ce75e680701d99
Felix Wagner <post@felixwagner.eu>
2026-03-31 20:36:05 +0200

    feat: add contact texts interface to contact.ts to replace double structures in html template prospectively

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -6,7 +6,7 @@ import { MatInput, MatError } from '@angular/material/input';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { merge } from 'rxjs';
+import { merge, map } from 'rxjs';
 
 
 @Component({
@@ -20,6 +20,8 @@ import { merge } from 'rxjs';
 export class Contact {
   private languageService = inject(LanguageService);
   language$ = this.languageService.language$;
+
+  readonly texts$ = this.language$.pipe(map((language) => language === 'de' ? germanTexts: englishTexts))
 
   readonly name = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(512)]);
   readonly email = new FormControl('', [Validators.required, Validators.email]);
@@ -83,3 +85,51 @@ export class Contact {
     }   
   }
 }
+
+export interface ContactTexts {
+  title: string;
+  subtitle: string;
+  nameLabel: string;
+  emailLabel: string;
+  emailPlaceholder: string;
+  messageLabel: string;
+  nameRequiredError: string;
+  emailRequiredError: string;
+  messageRequiredError: string;
+  privacyPrefix: string;
+  privacyLink: string;
+  privacySuffix: string;
+  submit: string;
+};
+
+export const germanTexts: ContactTexts = {
+  title: "Kontakt",
+  subtitle: "Lassen Sie uns ins Gespräch kommen!",
+  nameLabel: "Ihr Name",
+  emailLabel: "Ihre E-Mail Adresse",
+  emailPlaceholder: "kontakt@domain.de",
+  messageLabel: "Ihre Nachricht",
+  nameRequiredError: "Ihr Name ist erforderlich",
+  emailRequiredError: "Ihre E-Mail Adresse ist erforderlich",
+  messageRequiredError: "Ihre Nachricht ist erforderlich",
+  privacyPrefix: "Ich habe die ",
+  privacyLink: "Datenschutzerklärung",
+  privacySuffix: " gelesen und stimme der Verarbeitung meiner Daten wie beschrieben zu.",
+  submit: "Nachricht abschicken",
+};
+
+export const englishTexts: ContactTexts = {
+  title: "Contact",
+  subtitle: "Let us get in touch!",
+  nameLabel: "Your name",
+  emailLabel: "Your email",
+  emailPlaceholder: "contact@domain.com",
+  messageLabel: "Your message",
+  nameRequiredError: "Your name is required",
+  emailRequiredError: "Your email is required",
+  messageRequiredError: "Your message is required",
+  privacyPrefix: "I read the ",
+  privacyLink: "privacy policy",
+  privacySuffix: " and agree to the processing of my data as outlined.",
+  submit: "Send message",
+};
