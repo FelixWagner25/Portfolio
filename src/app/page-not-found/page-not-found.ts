c560a2fe7b7075c7f3bf71b92835598bbfba5901
Felix Wagner <post@felixwagner.eu>
2026-04-07 11:40:18 +0200

    feat: style page-not-found page

--- src/app/page-not-found/page-not-found.ts
+++ src/app/page-not-found/page-not-found.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterMinorPage } from "../shared/footer-minor-side/footer-minor-side";
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [FooterMinorPage],
   templateUrl: './page-not-found.html',
   styleUrl: './page-not-found.scss',
 })
