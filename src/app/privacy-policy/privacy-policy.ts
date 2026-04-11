94a482c53e90d186b2e74e340713c163605f5668
Felix Wagner <post@felixwagner.eu>
2026-04-11 14:16:36 +0200

    feat: add english translation of german privacy policy to privacy policy page

--- src/app/privacy-policy/privacy-policy.ts
+++ src/app/privacy-policy/privacy-policy.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { FooterMinorSide } from '../shared/footer-minor-side/footer-minor-side';
+import { AsyncPipe } from '@angular/common';
+import { LanguageService } from '../shared/services/language.service';
 
 @Component({
   standalone: true,
   selector: 'app-privacy-policy',
-  imports: [FooterMinorSide],
+  imports: [FooterMinorSide, AsyncPipe],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
 export class PrivacyPolicy {
-
+  private languageService = inject(LanguageService);
+  language$ = this.languageService.language$;
 }
