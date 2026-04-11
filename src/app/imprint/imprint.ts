e9b6d97428d0e6754a4c196a2da1055f539f78be
Felix Wagner <post@felixwagner.eu>
2026-04-11 14:07:38 +0200

    fix: separate english and german impressum text consistently

--- src/app/imprint/imprint.ts
+++ src/app/imprint/imprint.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FooterMinorSide } from '../shared/footer-minor-side/footer-minor-side';
+import { AsyncPipe } from '@angular/common';
+import { LanguageService } from '../shared/services/language.service';
 
 @Component({
   selector: 'app-imprint',
-  imports: [FooterMinorSide],
+  imports: [FooterMinorSide, AsyncPipe],
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss',
 })
 export class Imprint {
-
+  private languageService = inject(LanguageService);
+  language$ = this.languageService.language$;
 }
