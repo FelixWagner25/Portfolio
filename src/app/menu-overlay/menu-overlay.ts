1b1374334e3b66c37f41b9693fc070b1656001e6
Felix Wagner <post@felixwagner.eu>
2026-04-14 19:47:06 +0200

    feat: toggle menu overlay by clicking on menu button in header

--- src/app/menu-overlay/menu-overlay.ts
+++ src/app/menu-overlay/menu-overlay.ts
@@ -3,11 +3,11 @@ import { FooterMinorSide } from '../shared/footer-minor-side/footer-minor-side';
 import { RouterLink } from "@angular/router";
 
 @Component({
+  standalone: true,
   selector: 'app-menu-overlay',
   imports: [FooterMinorSide, RouterLink],
   templateUrl: './menu-overlay.html',
   styleUrl: './menu-overlay.scss',
 })
 export class MenuOverlay {
-
 }
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,15 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { AppLanguage, LanguageService } from '../services/language.service';
 import { AsyncPipe } from '@angular/common';
-import { MatMenu, MatMenuTrigger, MatMenuItem } from '@angular/material/menu';
-import { MatIconButton } from '@angular/material/button';
 import { RouterLink } from "@angular/router";
+import { MenuOverlay } from '../../menu-overlay/menu-overlay';
 
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [AsyncPipe, MatMenuTrigger, MatIconButton, MatMenu, MatMenuItem, RouterLink],
+  imports: [AsyncPipe, RouterLink, MenuOverlay],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
@@ -17,8 +16,18 @@ export class Header {
   private languageService = inject(LanguageService);
   language$ = this.languageService.language$;
 
+  public showOverlay: boolean = false;
+
   setLanguage(language: AppLanguage){
     this.languageService.setLanguage(language);
     console.log(language);
+  }
+
+  toggleMenuOverlay(){
+    if (this.showOverlay == false) {
+      this.showOverlay = true;
+    } else if (this.showOverlay == true){
+      this.showOverlay = false
+    }
   }
 }
