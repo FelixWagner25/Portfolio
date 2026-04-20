434214bdb81673e246cb4b0f42dd20a6273ac082
Felix Wagner <post@felixwagner.eu>
2026-04-20 18:20:30 +0200

    feat: add german texts to menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -2,18 +2,38 @@ import { Component, inject } from '@angular/core';
 import { FooterMinorSide } from '../shared/footer-minor-side/footer-minor-side';
 import { RouterLink } from "@angular/router";
 import { MenuService } from '../shared/services/menu.service';
+import { LanguageService } from '../shared/services/language.service';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-menu',
-  imports: [FooterMinorSide, RouterLink],
+  imports: [FooterMinorSide, RouterLink, AsyncPipe],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
 export class Menu {
   private menuService = inject(MenuService);
+  private languageService = inject(LanguageService);
+  language$ = this.languageService.language$;
+
+  readonly texts$ = this.language$.pipe(map((language) => language === 'de' ? germanTexts: englishTexts));
+
 
   closeMenu(){
     this.menuService.setShowMenu(false);
   }
+}
+
+export const germanTexts = {
+  aboutMe: "Über mich",
+  mySkills: "Fähigkeiten",
+  myPortfolio: "Portfolio",
+}
+
+export const englishTexts = {
+  aboutMe: "About me",
+  mySkills: "My skills",
+  myPortfolio: "Portfolio",
 }
