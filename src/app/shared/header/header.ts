0d24475db2a4c32a6599a5ff7bc38165171f0d15
Felix Wagner <post@felixwagner.eu>
2026-04-20 12:13:48 +0200

    feat: organize menu toggle

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { AppLanguage, LanguageService } from '../services/language.service';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -14,10 +15,24 @@ export class Header {
   private languageService = inject(LanguageService);
   language$ = this.languageService.language$;
 
+  private router = inject(Router);
+  private returnUrl = "";
+
   public showOverlay: boolean = false;
 
   setLanguage(language: AppLanguage){
     this.languageService.setLanguage(language);
     console.log(language);
+  }
+
+  toggleMenu(){
+    this.showOverlay = !this.showOverlay;
+    if (this.showOverlay) {
+      this.returnUrl = this.router.url
+      console.log(this.returnUrl);
+      this.router.navigate(['/menu']);
+    } else {
+      this.router.navigate([this.returnUrl]);
+    }
   }
 }
