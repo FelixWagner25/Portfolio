2a9e4b1f2deded75376c77c83e6ab90e7953a4fe
Felix Wagner <post@felixwagner.eu>
2026-04-29 12:08:19 +0200

    refactor: remove redundant image frame animation code from landing page component

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import { scrollDownArrowImages } from '../shared/constants/image-srcs';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,32 +8,4 @@ import { scrollDownArrowImages } from '../shared/constants/image-srcs';
   styleUrl: './landing-page.scss',
 })
 export class LandingPage {
-  private index: number = 2;
-  private animationInterval?: ReturnType<typeof setInterval>;
-  public arrowImg = scrollDownArrowImages[this.index];
-
-  constructor(private cdr: ChangeDetectorRef){}
-
-  ngOnInit(){
-    this.animateScrollDownArrow();
-  }
-
-  ngOnDestroy(){
-    if (this.animationInterval){
-      clearInterval(this.animationInterval);
-    }
-  }
-
-  animateScrollDownArrow(){
-    this.animationInterval = setInterval(() => {
-      this.index = this.index % scrollDownArrowImages.length;
-      this.arrowImg = scrollDownArrowImages[this.index];
-      this.index += 1;
-      this.cdr.detectChanges();
-    }, 500);
-  }
-
-   constructArrowBackgroundUrl(){
-    return this.arrowImg === 'none' ? 'none': `url(${this.arrowImg})`
-  }
 }
