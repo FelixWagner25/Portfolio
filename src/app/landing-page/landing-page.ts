4eb9bed274e60b80d7ab97c0e0aad918664380a7
Felix Wagner <post@felixwagner.eu>
2026-05-05 12:29:55 +0200

    feat: add german translations to landing page

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { LanguageService } from '../shared/services/language.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
 export class LandingPage {
+    private languageService = inject(LanguageService);
+    language$ = this.languageService.language$;
 }
+
