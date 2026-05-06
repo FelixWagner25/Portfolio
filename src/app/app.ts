4a13a35c1bf5a17d68a62d2737e3963dd2d340e3
Felix Wagner <post@felixwagner.eu>
2026-05-06 11:27:25 +0200

    refactor: remove appearently redundant aos refresh and belonging dependencies from app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
-import { Component, inject, signal, AfterViewInit } from '@angular/core';
-import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { Component, inject, signal} from '@angular/core';
+import { RouterOutlet} from '@angular/router';
 import { Header } from './shared/header/header';
 import { ViewportScroller } from '@angular/common';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,19 +16,13 @@ export class App {
 
   private viewportScroller = inject(ViewportScroller);
 
-  constructor(
-    private router: Router
-  ){
+  constructor(  ){
     this.viewportScroller.setOffset([0,110]);
   }
 
   ngAfterViewInit(){
-    AOS.init();
-
-    this.router.events
-    .pipe(filter(event => event instanceof NavigationEnd))
-    .subscribe(() => {
-      AOS.refresh();
+    AOS.init({
+      duration: 600,
     });
   }
 }
